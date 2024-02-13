86345bfb9875bd4295b3fd9fafceb43c9689f2c9
FasihurRehman407 <bsef19m511@pucit.edu.pk>
2024-02-13 21:07:40 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // import BarCodeReader from "./BarCodeReader";
 
+import { useState } from "react";
 import Html5Plugin from "./Html5Plugin";
 
 function App() {
+  const [decodedResult, setDecodedResult] = useState(null);
   const onNewScanResult = (decodedText, decodedResult) => {
     console.log(decodedText, decodedResult);
+    setDecodedResult(decodedResult);
   };
   return (
     <div className="App">
       {/* <BarCodeReader /> */}
+      {decodedResult && <div>{decodedResult.decodedText}</div>}
       <Html5Plugin
         fps={10}
         qrbox={250}
