d0872ed16387d7344d8c11b7a4c3e5106af3292c
FasihurRehman407 <bsef19m511@pucit.edu.pk>
2024-02-13 17:22:57 +0500

    Change

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import BarCodeReader from "./BarCodeReader";
+
+function App() {
+  return (
+    <div className="App">
+      <BarCodeReader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BarCodeReader.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect, useRef } from "react";
+import Quagga from "@ericblade/quagga2";
+
+function BarCodeReader() {
+  const [scanning, setScanning] = useState(false);
+  const [result, setResult] = useState("");
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (scanning) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          videoRef.current.srcObject = stream;
+          initQuagga();
+        })
+        .catch((err) => console.error("Error accessing camera:", err));
+    } else if (Quagga.initialized) {
+      Quagga.stop();
+    }
+
+    return () => {
+      // Clean up
+      Quagga.stop();
+    };
+  }, [scanning]);
+
+  const initQuagga = () => {
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: videoRef.current,
+          constraints: {
+            facingMode: "environment", // Use rear camera
+          },
+        },
+        decoder: {
+          readers: [
+            "code_128_reader",
+            "ean_reader",
+            "upc_reader",
+            "upc_e_reader",
+          ], // barcode formats
+        },
+      },
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("Quagga initialization finished. Ready to start");
+        Quagga.start();
+      }
+    );
+
+    Quagga.onDetected((data) => {
+      console.log(data.codeResult.code);
+      setResult(data.codeResult.code);
+      setScanning(false);
+    });
+  };
+
+  const handleStartScan = () => {
+    setScanning(true);
+    setResult("");
+  };
+
+  return (
+    <div>
+      <button onClick={handleStartScan}>Start Scan</button>
+      {scanning && (
+        <video
+          id="myVideo"
+          style={{
+            width: "400px",
+            height: "400px",
+            objectFit: "cover",
+          }}
+          ref={videoRef}
+          autoPlay
+        />
+      )}
+      <input type="text" value={result} placeholder="BarCodeResult" readOnly />
+    </div>
+  );
+}
+
+export default BarCodeReader;
